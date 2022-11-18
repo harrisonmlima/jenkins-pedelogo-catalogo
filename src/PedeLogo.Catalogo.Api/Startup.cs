f4c4ec3651119bd2325a2c1d856023c0ba404059
root <root@DESKTOP-FGB7N8H.localdomain>
2022-11-18 20:09:50 -0300

    a

--- src/PedeLogo.Catalogo.Api/Startup.cs
+++ src/PedeLogo.Catalogo.Api/Startup.cs
@@ -49,7 +49,7 @@ namespace PedeLogo.Catalogo.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    Version = "v3",
+                    Version = "v1",
                     Title = "API para cadastro de Produtos",
                     Description = "API para cadastro de Produtos"
                 });
